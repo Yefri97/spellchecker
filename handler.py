58fb33abc2d9b0ad5d34fd0b5219531dc2e1f62a
Yeferson Gaitan Gomez <yefri.gaitan97@gmail.com>
2020-10-14 14:30:40 -0500

    Third task: endpoint with serverless

--- handler.py
+++ handler.py
@@ -2,21 +2,10 @@ import json
 from spell_checker import spell_check_sentence
 
 def spell_check(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-    
-    
-
-
-
-
-
+	text = json.loads(event['body'])['text']
+	body = { "texto": spell_check_sentence(text) }
+	response = {
+		"statusCode": 200,
+		"body": json.dumps(body),
+	}
+	return response
