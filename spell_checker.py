31186efb536569e4998dafe6ddbfc4a41611e09b
Yeferson Gaitan Gomez <yefri.gaitan97@gmail.com>
2020-10-14 10:48:12 -0500

    Second task: Mejorar el algoritmo

--- spell_checker.py
+++ spell_checker.py
@@ -46,18 +46,47 @@ def two_length_edit(word):
 	'''Función no alterda por el ataque'''
 	return [e2 for e1 in one_length_edit(word) for e2 in one_length_edit(e1)]
 
+""" NEW FUNCTIONS """
+# return a word with an accents in word[pos] if pos is a vowel
+def accent(word, pos):
+	replacements = {
+		'a': 'á',
+		'e': 'é',
+		'i': 'í',
+		'o': 'ó',
+		'u': 'ú'
+	};
+	if word[pos] in replacements:
+		return word[:pos] + replacements[word[pos]] + word[pos+1:]
+	elif word[pos].lower() in replacements:
+		return word[:pos] + replacements[word[pos].lower()].upper() + word[pos+1:]
+	else:
+		return word
 
+# return a collection of words with accents
+def put_accents(word):
+	return [accent(word, i) for i in range(len(word))]
+
+# return the word without upper letters and punctuation signs
+def normalize(word):
+	return word.lower().strip('.,?¿')
+""" END NEW FUNCIONTS"""
+
+# return a set of words that exist in the dict or in the book
 def filter_real_words(words):
-	return set(word for word in words if word in WORDS_INDEX)
+	return set(word for word in words if normalize(word) in WORDS_INDEX)
 
-
+# return corrections with at most two modifications and accents fixes
 def possible_corrections(word):
 	single_word_possible_corrections = filter_real_words([word])
+	accents_corrections = filter_real_words(put_accents(word))
 	one_length_edit_possible_corrections = filter_real_words(one_length_edit(word))
 	two_length_edit_possible_corrections = filter_real_words(two_length_edit(word))
 	no_correction_at_all = set([word])
 	if single_word_possible_corrections:
 		return single_word_possible_corrections
+	if accents_corrections: # a tilde error is much more likely than a one-letter change
+		return accents_corrections
 	elif one_length_edit_possible_corrections:
 		return one_length_edit_possible_corrections
 	elif two_length_edit_possible_corrections:
@@ -65,23 +94,21 @@ def possible_corrections(word):
 	else:
 		return no_correction_at_all
 
-
+# number of times the word appears in the dictionary and book
 def language_model(word):
 	N = sum(WORDS_INDEX.values())
-	return WORDS_INDEX.get(word, 0) / N
+	return WORDS_INDEX.get(normalize(word), 0) / N
 
-
+# Of all the possibilities choose the most common word
 def spell_check_word(word):
 	return max(possible_corrections(word), key=language_model)
 
-
+# break the sentence into words to check each one
 def spell_check_sentence(sentence):
-	lower_cased_sentence = sentence.lower()
-	stripped_sentence = list(map(lambda x : x.strip('.,?¿'), lower_cased_sentence.split()))
-	checked = list(map(spell_check_word, stripped_sentence))
+	checked = list(map(spell_check_word, sentence.split()))
 	return ' '.join(checked)
 
-
+# test several cases
 def test_spell_check_sentence():
 	sentence = 'fabor guardar cilencio para no molestar'
 	assert 'favor guardar silencio para no molestar' == spell_check_sentence(sentence) 
@@ -118,4 +145,9 @@ for word in WORDS:
 	else:
 		WORDS_INDEX[word] = 1
 
-test_spell_check_sentence()
+""" Aditional test case """
+# No problem with punctuation marks
+# No problem with capitalization
+# Good handling of accents
+sentence = '¿Él?, Nop era una persona de fiar, pues era un mentirozo todo el dia.'
+assert '¿Él?, No era una persona de fiar, pues era un mentiroso todo el día.' == spell_check_sentence(sentence)
